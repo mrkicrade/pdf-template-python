20e4c0eb4f23439195660704a4cc6375c55ea848
mrkicrade <mrkic78@gmail.com>
2023-05-01 13:33:56 +0200

    Finally commit

--- main.py
+++ main.py
@@ -2,19 +2,38 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation='P', unit='mm', format='A4')
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv('topics.csv')
+# print(df)
 
 for index, row in df.iterrows():
     pdf.add_page()
 
-    pdf.set_font(family='Arial', style='B', size=24)
+    # Set the header
+    pdf.set_font(family='Times', style='B', size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
     # pdf.line(x1, y1, x2, y2)
-    pdf.line(10, 21, 200, 21)
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
+
+    # Set the footer
+    pdf.ln(265)
+    pdf.set_font(family='Times', style='I', size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
 
     for i in range(row['Pages'] - 1):
         pdf.add_page()
+
+        # Set the footer
+        pdf.ln(277)
+        pdf.set_font(family='Times', style='I', size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
+
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output('output.pdf')
